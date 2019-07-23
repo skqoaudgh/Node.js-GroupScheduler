051cef366e15bd06de818fc71dd2d806c0a07253
myungho <skqoaudgh2@hanmail.net>
2019-07-23 21:14:09 +0900

    public 파일들을 각 페이지에 따라 분류. 일정 생성 페이지 추가

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', (req, res, next) => {
     res.render('index.ejs');
 });
 
+app.get('/create', (req, res, next) => {
+    res.render('create.ejs');
+});
+
 app.listen(3000, () => {
     console.log('express server is opened on port 3000.'); 
 });
