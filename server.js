5da22e4cdf38ed1b26b7c04251a8607c375547f8
myungho <skqoaudgh2@hanmail.net>
2019-07-24 19:10:31 +0900

    사용자의 입력을 POST로 받아 처리할 수 있도록 설정

--- server.js
+++ server.js
@@ -5,12 +5,19 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res, next) => {
     res.render('index.ejs');
 });
 
 app.get('/create', (req, res, next) => {
     res.render('create.ejs');
+});
+
+app.post('/create', (req, res ,next) => {
+    console.log(req.body);
 });
 
 app.listen(3000, () => {
